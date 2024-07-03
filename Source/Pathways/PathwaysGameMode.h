225e02c39830235c1eda7093535dceeb3357848f
gotagetfood <96447537+gotagetfood@users.noreply.github.com>
2024-07-02 19:16:11 -0600

    the project start
    its the start

---
+++ Source/Pathways/PathwaysGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PathwaysGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APathwaysGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APathwaysGameMode();
+};
+
+
+
